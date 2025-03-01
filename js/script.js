15827fe2277c8de2d8f5f13bb32ee4824486def4
BothOfYou <117015056+BothOfYou@users.noreply.github.com>
2025-03-01 15:37:49 +0100

    project structure, adde style fixed heading

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// const validUrl = ["https://bothofyou.github.io/GdG-Prozesse/navbar.html", "https://bothofyou.github.io/GdG-Prozesse/project_1.html", "https://bothofyou.github.io/GdG-Prozesse/project_2.html", "https://bothofyou.github.io/GdG-Prozesse/project_3.html","https://bothofyou.github.io/GdG-Prozesse/project_4.html", "https://bothofyou.github.io/GdG-Prozesse/project_5.html"];
-
-// if(!validUrl.includes(window.location.href)) {
-//     location.replace("https://bothofyou.github.io/GdG-Prozesse/navbar.html");
-// }
-
-
-
 const loadPage = pageIndex => {
     const currenPageElement = document.querySelector('#currentPage');
     var redirectFile = `./main.html`;
@@ -22,27 +14,3 @@ const loadPage = pageIndex => {
         currenPageElement.innerHTML = htmlFile;
     })
 }
-
-// function loadPage(pageIndex) {
-   
-//     switch(pageIndex) {
-//         case 0:
-//             currenPageElement.innerHTML("afterbegin", "./main.html");
-//             break;
-//         case 1:
-//             currenPageElement.insertAdjacentHTML("afterbegin", "./project_1.html");
-//             break;
-//         case 2:
-//             currenPageElement.insertAdjacentHTML("afterbegin", "./project_1.html");
-
-//             break;
-//         case 3:
-//             break;
-//         case 4:
-//             break;
-//         case 5:
-//             break;
-//         default:
-//             currenPageElement.insertAdjacentHTML("afterbegin", "./main.html");
-//     }
-// }
